e81988bdf95bdc8a1a9986872c1087e6aad2726b
Jed Brown <jedbrown@mcs.anl.gov>
2013-11-07 09:39:47 -0700

    PCPBJacobi: add PCApply_PBJacobi_N (not unrolled/optimized)

--- src/ksp/pc/impls/pbjacobi/pbjacobi.c
+++ src/ksp/pc/impls/pbjacobi/pbjacobi.c
@@ -192,6 +192,34 @@ static PetscErrorCode PCApply_PBJacobi_7(PC pc,Vec x,Vec y)
   ierr = PetscLogFlops(91*m);CHKERRQ(ierr); /* 2*bs2 - bs */
   PetscFunctionReturn(0);
 }
+
+static PetscErrorCode PCApply_PBJacobi_N(PC pc,Vec x,Vec y)
+{
+  PC_PBJacobi       *jac = (PC_PBJacobi*)pc->data;
+  PetscErrorCode    ierr;
+  PetscInt          i,j,k,m = jac->mbs,bs=jac->bs;
+  const MatScalar   *diag = jac->diag;
+  const PetscScalar *xx;
+  PetscScalar       *yy;
+
+  PetscFunctionBegin;
+  ierr = VecGetArrayRead(x,&xx);CHKERRQ(ierr);
+  ierr = VecGetArray(y,&yy);CHKERRQ(ierr);
+  for (i=0; i<m; i++) {
+    for (j=0; j<bs; j++) yy[i*bs+j] = 0.;
+    for (j=0; j<bs; j++) {
+      for (k=0; k<bs; k++) {
+        yy[i*bs+k] += diag[k+bs*j]*xx[i*bs+j];
+      }
+    }
+    diag += bs*bs;
+  }
+  ierr = VecRestoreArrayRead(x,&xx);CHKERRQ(ierr);
+  ierr = VecRestoreArray(y,&yy);CHKERRQ(ierr);
+  ierr = PetscLogFlops(m*bs*(2*bs-1));CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
 /* -------------------------------------------------------------------------- */
 static PetscErrorCode PCSetUp_PBJacobi(PC pc)
 {
@@ -232,7 +260,7 @@ static PetscErrorCode PCSetUp_PBJacobi(PC pc)
     pc->ops->apply = PCApply_PBJacobi_7;
     break;
   default:
-    SETERRQ1(PetscObjectComm((PetscObject)pc),PETSC_ERR_SUP,"not supported for block size %D",jac->bs);
+    pc->ops->apply = PCApply_PBJacobi_N;
   }
   PetscFunctionReturn(0);
 }
