912f1174eb5036ee569f2c5b056d073308071c91
Matthew G. Knepley <knepley@gmail.com>
2014-02-27 16:26:45 -0600

    DMDA: Fix bug in SetFromOptions - If the interpolation type was Q0, the size of unused dimensions was screwed up

--- src/dm/impls/da/dacreate.c
+++ src/dm/impls/da/dacreate.c
@@ -90,15 +90,19 @@ PetscErrorCode  DMSetFromOptions_DA(DM da)
     } else {
       dd->M = 1 + dd->refine_x*(dd->M - 1);
     }
-    if (dd->by == DM_BOUNDARY_PERIODIC || dd->interptype == DMDA_Q0) {
-      dd->N = dd->refine_y*dd->N;
-    } else {
-      dd->N = 1 + dd->refine_y*(dd->N - 1);
+    if (dd->dim > 1) {
+      if (dd->by == DM_BOUNDARY_PERIODIC || dd->interptype == DMDA_Q0) {
+        dd->N = dd->refine_y*dd->N;
+      } else {
+        dd->N = 1 + dd->refine_y*(dd->N - 1);
+      }
     }
-    if (dd->bz == DM_BOUNDARY_PERIODIC || dd->interptype == DMDA_Q0) {
-      dd->P = dd->refine_z*dd->P;
-    } else {
-      dd->P = 1 + dd->refine_z*(dd->P - 1);
+    if (dd->dim > 2) {
+      if (dd->bz == DM_BOUNDARY_PERIODIC || dd->interptype == DMDA_Q0) {
+        dd->P = dd->refine_z*dd->P;
+      } else {
+        dd->P = 1 + dd->refine_z*(dd->P - 1);
+      }
     }
     da->levelup++;
     if (da->levelup - da->leveldown >= 0) {
