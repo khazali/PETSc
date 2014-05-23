f8f62dc51cf570c428dc70bea70875f94543eb68
Paul Eller <eller3@illinois.edu>
2014-05-23 16:17:11 -0500

    Initial implementation of tbb, appears to work correctly based on tests using Vec examples - Made sure files were added.

---
+++ src/sys/threadcomm/impls/tbb/tctbb.cxx
@@ -0,0 +1,42 @@
+#include <../src/sys/threadcomm/impls/tbb/tctbbimpl.h>
+#include "blocked_range.h"
+#include "parallel_for.h"
+#include "task_scheduler_init.h"
+
+using namespace tbb;
+
+class TBBRunKernel {
+  PetscThreadCommJobCtx job;
+
+public:
+  void operator()(blocked_range<size_t>& r) const {
+    int trank= r.begin();
+    job->job_status[trank] = THREAD_JOB_RECIEVED;
+    PetscRunKernel(trank,job->nargs,job);
+    job->job_status[trank]= THREAD_JOB_COMPLETED;
+  }
+
+  TBBRunKernel(PetscThreadCommJobCtx ijob) : job(ijob) {}
+};
+
+#undef __FUNCT__
+#define __FUNCT__ "PetscThreadCommCreate_TBB"
+PETSC_EXTERN PetscErrorCode PetscThreadCommCreate_TBB(PetscThreadComm tcomm)
+{
+  PetscErrorCode ierr;
+
+  PetscFunctionBegin;
+  ierr = PetscStrcpy(tcomm->type,TBB);CHKERRQ(ierr);
+  tcomm->ops->runkernel = PetscThreadCommRunKernel_TBB;
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "PetscThreadCommRunKernel_TBB"
+PetscErrorCode PetscThreadCommRunKernel_TBB(PetscThreadComm tcomm,PetscThreadCommJobCtx job)
+{
+  PetscFunctionBegin;
+  task_scheduler_init init(tcomm->nworkThreads);
+  parallel_for(blocked_range<size_t>(0,tcomm->nworkThreads,1),TBBRunKernel(job));
+  PetscFunctionReturn(0);
+}
---
+++ src/sys/threadcomm/impls/tbb/tctbbimpl.h
@@ -0,0 +1,8 @@
+#if !defined(__TCTBBIMPLH)
+#define __TCTBBIMPLH
+
+#include <petsc-private/threadcommimpl.h>
+PETSC_EXTERN PetscErrorCode PetscThreadCommCreate_TBB(PetscThreadComm);
+extern PetscErrorCode PetscThreadCommRunKernel_TBB(PetscThreadComm,PetscThreadCommJobCtx);
+
+#endif
