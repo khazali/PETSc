3d00f54b2fefa4f533a582dcf55bfa6256fdb895
Michael Lange <michael.lange@imperial.ac.uk>
2014-11-10 07:25:09 +0000

    Plex-Case: Build Plex topology from ascii .cas files

--- src/dm/impls/plex/plexcase.c
+++ src/dm/impls/plex/plexcase.c
@@ -32,7 +32,6 @@ PetscErrorCode DMPlexCreateCase_ReadSection(PetscViewer viewer, CaseSection *s)
 {
   char           buffer[PETSC_MAX_PATH_LEN];
   int            snum;
-  PetscInt       i;
   PetscErrorCode ierr;
 
   PetscFunctionBegin;
@@ -53,22 +52,46 @@ PetscErrorCode DMPlexCreateCase_ReadSection(PetscViewer viewer, CaseSection *s)
     break;
   case 10:   /* Vertices */
     ierr = DMPlexCaseRead(viewer, buffer, ')');CHKERRQ(ierr);
-    snum = sscanf(buffer, "(%d %x %x %d %d)", &(s->zoneID), &(s->first), &(s->last), &(s->type), &(s->nd));CHKERRQ(snum!=5);
+    snum = sscanf(buffer, "(%x %x %x %d %d)", &(s->zoneID), &(s->first), &(s->last), &(s->type), &(s->nd));CHKERRQ(snum!=5);
     ierr = DMPlexCaseRead(viewer, buffer, ')');CHKERRQ(ierr);
     break;
   case 12:   /* Cells */
     ierr = DMPlexCaseRead(viewer, buffer, ')');CHKERRQ(ierr);
-    snum = sscanf(buffer, "(%d", &(s->zoneID));CHKERRQ(snum!=1);
-    if (s->zoneID == 0) {snum = sscanf(buffer, "(%d %x %x %d)", &(s->zoneID), &(s->first), &(s->last), &(s->nd));CHKERRQ(snum!=4);}
-    else {snum = sscanf(buffer, "(%d %x %x %d %d)", &(s->zoneID), &(s->first), &(s->last), &(s->type), &(s->nd));CHKERRQ(snum!=5);}
+    snum = sscanf(buffer, "(%x", &(s->zoneID));CHKERRQ(snum!=1);
+    if (s->zoneID == 0) {snum = sscanf(buffer, "(%x %x %x %d)", &(s->zoneID), &(s->first), &(s->last), &(s->nd));CHKERRQ(snum!=4);}
+    else {snum = sscanf(buffer, "(%x %x %x %d %d)", &(s->zoneID), &(s->first), &(s->last), &(s->type), &(s->nd));CHKERRQ(snum!=5);}
     ierr = DMPlexCaseRead(viewer, buffer, ')');CHKERRQ(ierr);
     break;
   case 13:   /* Faces */
     ierr = DMPlexCaseRead(viewer, buffer, ')');CHKERRQ(ierr);
-    snum = sscanf(buffer, "(%d", &(s->zoneID));CHKERRQ(snum!=1);
-    if (s->zoneID == 0) {snum = sscanf(buffer, "(%d %x %x %d)", &(s->zoneID), &(s->first), &(s->last), &(s->nd));CHKERRQ(snum!=4);}
-    else {snum = sscanf(buffer, "(%d %x %x %d %d)", &(s->zoneID), &(s->first), &(s->last), &(s->type), &(s->nd));CHKERRQ(snum!=5);}
-    ierr = DMPlexCaseRead(viewer, buffer, ')');CHKERRQ(ierr);
+    snum = sscanf(buffer, "(%x", &(s->zoneID));CHKERRQ(snum!=1);
+    if (s->zoneID == 0) {
+      snum = sscanf(buffer, "(%x %x %x %d)", &(s->zoneID), &(s->first), &(s->last), &(s->nd));CHKERRQ(snum!=4);
+      ierr = DMPlexCaseRead(viewer, buffer, ')');CHKERRQ(ierr);
+    } else {
+      PetscInt f, e, numEntries, numFaces, *face_array;
+      int      entry;
+      snum = sscanf(buffer, "(%x %x %x %d %d)", &(s->zoneID), &(s->first), &(s->last), &(s->type), &(s->nd));CHKERRQ(snum!=5);
+      switch (s->nd) {
+      case 0: SETERRQ(PETSC_COMM_SELF, PETSC_ERR_ARG_WRONG, "Mixed faces in Case files are not supported");
+      case 2: numEntries = 2 + 2; break;  /* linear */
+      case 3: numEntries = 2 + 3; break;  /* triangular */
+      case 4: numEntries = 2 + 4; break;  /* quadrilateral */
+      default: SETERRQ(PETSC_COMM_SELF, PETSC_ERR_ARG_WRONG, "Unknown face type in Case file");
+      }
+      ierr = DMPlexCaseRead(viewer, buffer, '(');CHKERRQ(ierr);
+      ierr = DMPlexCaseRead(viewer, buffer, '\n');CHKERRQ(ierr);
+      numFaces = s->last-s->first + 1;
+      ierr = PetscMalloc1(numEntries*numFaces, &face_array);CHKERRQ(ierr);
+      for (f = 0; f < numFaces; f++) {
+        for (e = 0; e < numEntries; e++) {
+          ierr = PetscViewerRead(viewer, buffer, 1, PETSC_STRING);CHKERRQ(ierr);
+          snum = sscanf(buffer, "%x", &entry);CHKERRQ(snum!=1);
+          face_array[f*numEntries + e] = entry;
+        }
+      }
+      s->data = face_array;
+    }
   default:
     ierr = DMPlexCaseRead(viewer, buffer, '\n');CHKERRQ(ierr);
   }
@@ -100,7 +123,9 @@ PetscErrorCode DMPlexCreateCase_ReadSection(PetscViewer viewer, CaseSection *s)
 PetscErrorCode DMPlexCreateCase(MPI_Comm comm, PetscViewer viewer, PetscBool interpolate, DM *dm)
 {
   PetscMPIInt    rank;
-  PetscInt       dim = -1, c, numCells, numVertices, numCellVertices;
+  PetscInt       c, f, v, dim = -1, numCells = -1, numVertices = -1, numFaces = -1;
+  PetscInt       numCellVertices = -1 , numFaceEntries = -1, numFaceVertices = -1;
+  PetscInt      *faces, *cellVertices;
   PetscErrorCode ierr;
 
   PetscFunctionBegin;
@@ -109,22 +134,22 @@ PetscErrorCode DMPlexCreateCase(MPI_Comm comm, PetscViewer viewer, PetscBool int
   if (!rank) {
     PetscInt index;
     do {
-      CaseSection sec;
-      ierr = DMPlexCreateCase_ReadSection(viewer, &sec);CHKERRQ(ierr);
-      index = sec.index;
+      CaseSection s;
+      ierr = DMPlexCreateCase_ReadSection(viewer, &s);CHKERRQ(ierr);
+      index = s.index;
       switch (index) {
       case -1: break;  /* End-of-file */
       case 0: break;   /* Comment */
-      case 2:          /* Dimension */
-        dim = sec.nd;
+      case 2:          /* Dimsion */
+        dim = s.nd;
         break;
-      case 10      :   /* Vertices */
-        if (sec.zoneID == 0) numVertices = sec.last;
+      case 10:         /* Vertices */
+        if (s.zoneID == 0) numVertices = s.last;
         break;
       case 12:         /* Cells */
-        if (sec.zoneID == 0) numCells = sec.last;
+        if (s.zoneID == 0) numCells = s.last;
         else {
-          switch (sec.nd) {
+          switch (s.nd) {
           case 0: SETERRQ(PETSC_COMM_SELF, PETSC_ERR_ARG_WRONG, "Mixed elements in Case files are not supported");
           case 1: numCellVertices = 3; break;  /* triangular */
           case 2: numCellVertices = 4; break;  /* tetrahedral */
@@ -136,10 +161,22 @@ PetscErrorCode DMPlexCreateCase(MPI_Comm comm, PetscViewer viewer, PetscBool int
           }
         }
         break;
-      case 13:
+      case 13:         /* Faces */
+        if (s.zoneID == 0) numFaces = s.last - s.first + 1;
+        else {
+          if (s.nd == 0 || (numFaceEntries > 0 && s.nd != numFaceVertices)) {
+            SETERRQ(PETSC_COMM_SELF, PETSC_ERR_ARG_WRONG, "Mixed facets in Case files are not supported");
+          }
+          if (numFaces < 0) SETERRQ(PETSC_COMM_SELF, PETSC_ERR_ARG_WRONG, "No header stion for facets in Case file");
+          if (numFaceVertices < 0) {
+            numFaceVertices = s.nd;
+            numFaceEntries = numFaceVertices + 2;
+            ierr = PetscMalloc1(numFaces*numFaceEntries, &faces);CHKERRQ(ierr);
+          }
+          ierr = PetscMemcpy(&(faces[(s.first-1)*numFaceEntries]), s.data, (s.last-s.first+1)*numFaceEntries*sizeof(PetscInt));CHKERRQ(ierr);
+          ierr = PetscFree(s.data);CHKERRQ(ierr);
+        }
         break;
-      case 45: break;
-      default: SETERRQ1(PETSC_COMM_SELF, PETSC_ERR_ARG_WRONG, "Unknown section index in Case file: %d", index);
       }
     } while (index >= 0);
   }
@@ -154,7 +191,54 @@ PetscErrorCode DMPlexCreateCase(MPI_Comm comm, PetscViewer viewer, PetscBool int
     for (c = 0; c < numCells; ++c) {ierr = DMPlexSetConeSize(*dm, c, numCellVertices);CHKERRQ(ierr);}
   }
   ierr = DMSetUp(*dm);CHKERRQ(ierr);
-  /* Add cell-vertex connections */
+  if (!rank) {
+    /* Derive cell-vertex list from face-vertex and face-cell maps */
+    if (numCells < 0 || numCellVertices < 0) SETERRQ(PETSC_COMM_SELF, PETSC_ERR_ARG_WRONG, "Insufficent cell header information in Case file");
+    ierr = PetscMalloc1(numCells*numCellVertices, &cellVertices);CHKERRQ(ierr);
+    for (c = 0; c < numCells*numCellVertices; c++) cellVertices[c] = -1;
+    for (f = 0; f < numFaces; f++) {
+      PetscInt *cell;
+      const PetscInt cl = faces[f*numFaceEntries + numFaceVertices];
+      const PetscInt cr = faces[f*numFaceEntries + numFaceVertices + 1];
+      const PetscInt *face = &(faces[f*numFaceEntries]);
 
+      if (cl > 0) {
+        cell = &(cellVertices[(cl-1) * numCellVertices]);
+        for (v = 0; v < numFaceVertices; v++) {
+          PetscBool found = PETSC_FALSE;
+          for (c = 0; c < numCellVertices; c++) {
+            if (cell[c] < 0) break;
+            if (cell[c] == face[v]-1 + numCells) {found = PETSC_TRUE; break;}
+          }
+          if (!found) cell[c] = face[v]-1 + numCells;
+        }
+      }
+      if (cr > 0) {
+        cell = &(cellVertices[(cr-1) * numCellVertices]);
+        for (v = 0; v < numFaceVertices; v++) {
+          PetscBool found = PETSC_FALSE;
+          for (c = 0; c < numCellVertices; c++) {
+            if (cell[c] < 0) break;
+            if (cell[c] == face[v]-1 + numCells) {found = PETSC_TRUE; break;}
+          }
+          if (!found) cell[c] = face[v]-1 + numCells;
+        }
+      }
+    }
+    for (c = 0; c < numCells; c++) {
+      ierr = DMPlexSetCone(*dm, c, &(cellVertices[c*numCellVertices]));CHKERRQ(ierr);
+    }
+  }
+  ierr = DMPlexSymmetrize(*dm);CHKERRQ(ierr);
+  ierr = DMPlexStratify(*dm);CHKERRQ(ierr);
+  if (interpolate) {
+    DM idm = NULL;
+
+    ierr = DMPlexInterpolate(*dm, &idm);CHKERRQ(ierr);
+    ierr = DMDestroy(dm);CHKERRQ(ierr);
+    *dm  = idm;
+  }
+
+  ierr = PetscFree2(cellVertices, faces);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
