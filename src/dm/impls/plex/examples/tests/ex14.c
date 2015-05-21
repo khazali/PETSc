cb717170c46f6b270df38874f6449328d55b798a
Michael Lange <michael.lange@imperial.ac.uk>
2015-05-21 06:14:14 +0100

    Plex-ex14: Add mesh file input and a complex hybrid mesh test
    Thanks to Rupert Nash and the Nektar++ team for providing the test mesh.

--- src/dm/impls/plex/examples/tests/ex14.c
+++ src/dm/impls/plex/examples/tests/ex14.c
@@ -5,6 +5,7 @@ static char help[] = "Test for hybrid meshing capabilities\n";
 typedef struct {
   PetscInt      dim;                          /* The topological mesh dimension */
   PetscBool     interpolate;                  /* Generate intermediate mesh elements */
+  char          meshfile[PETSC_MAX_PATH_LEN]; /* Input mesh file */
   char          hdf5file[PETSC_MAX_PATH_LEN]; /* Export mesh to file (HDF5) */
 } AppCtx;
 
@@ -18,11 +19,13 @@ PetscErrorCode ProcessOptions(MPI_Comm comm, AppCtx *options)
   PetscFunctionBegin;
   options->dim               = 2;
   options->interpolate       = PETSC_FALSE;
+  options->meshfile[0]       = '\0';
   options->hdf5file[0]       = '\0';
 
   ierr = PetscOptionsBegin(comm, "", "Hybrid Meshing Problem Options", "DMPLEX");CHKERRQ(ierr);
   ierr = PetscOptionsInt("-dim", "The topological mesh dimension", "ex14.c", options->dim, &options->dim, NULL);CHKERRQ(ierr);
   ierr = PetscOptionsBool("-interpolate", "Generate intermediate mesh elements", "ex1.c", options->interpolate, &options->interpolate, NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsString("-meshfile", "Output HDF5 file", "ex14.c", options->meshfile, options->meshfile, PETSC_MAX_PATH_LEN, NULL);CHKERRQ(ierr);
   ierr = PetscOptionsString("-hdf5file", "Output HDF5 file", "ex14.c", options->hdf5file, options->hdf5file, PETSC_MAX_PATH_LEN, NULL);CHKERRQ(ierr);
   ierr = PetscOptionsEnd();
 
@@ -46,8 +49,9 @@ $  2 ----- 3
 #define __FUNCT__ "CreateMesh"
 PetscErrorCode CreateMesh(MPI_Comm comm, AppCtx *user, DM *dm)
 {
-  PetscInt       dim             = user->dim;
+  PetscInt       len, dim        = user->dim;
   PetscBool      interpolate     = user->interpolate;
+  const char    *filename        = user->meshfile;
   PetscMPIInt    rank, numProcs;
   PetscErrorCode ierr;
 
@@ -55,32 +59,37 @@ PetscErrorCode CreateMesh(MPI_Comm comm, AppCtx *user, DM *dm)
   ierr = MPI_Comm_rank(comm, &rank);CHKERRQ(ierr);
   ierr = MPI_Comm_size(comm, &numProcs);CHKERRQ(ierr);
 
-  /* Create a 2D quad-tri mesh */
-  ierr = DMPlexCreate(comm, dm);CHKERRQ(ierr);
-  ierr = DMSetType(*dm, DMPLEX);CHKERRQ(ierr);
-  ierr = DMSetDimension(*dm, 2);CHKERRQ(ierr);
-  if (dim == 2) {
-    /* Non-interpolated hybrid quad-tri mesh */
-    PetscInt    numPoints[2]         = {5, 2};
-    PetscInt    coneSize[7]          = {4, 3, 0, 0, 0, 0, 0};
-    PetscInt    cones[7]             = {2, 3, 4, 5,  4, 5, 6};
-    PetscInt    coneOrientations[7]  = {0, 0, 0, 0, 0, 0, 0};
-    PetscScalar vertexCoords[10]     = {0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.5, 1.5};
-    PetscInt    markerPoints[10]     = {2, 1, 3, 1, 4, 1, 5, 1, 6, 1};
-    PetscInt    p;
+  ierr = PetscStrlen(filename, &len);CHKERRQ(ierr);
+  if (len > 0) {
+    ierr = DMPlexCreateFromFile(comm, filename, interpolate, dm);CHKERRQ(ierr);
+  } else {
+    /* Create a 2D quad-tri mesh */
+    ierr = DMPlexCreate(comm, dm);CHKERRQ(ierr);
+    ierr = DMSetType(*dm, DMPLEX);CHKERRQ(ierr);
+    ierr = DMSetDimension(*dm, 2);CHKERRQ(ierr);
+    if (dim == 2) {
+      /* Non-interpolated hybrid quad-tri mesh */
+      PetscInt    numPoints[2]         = {5, 2};
+      PetscInt    coneSize[7]          = {4, 3, 0, 0, 0, 0, 0};
+      PetscInt    cones[7]             = {2, 3, 4, 5,  4, 5, 6};
+      PetscInt    coneOrientations[7]  = {0, 0, 0, 0, 0, 0, 0};
+      PetscScalar vertexCoords[10]     = {0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.5, 1.5};
+      PetscInt    markerPoints[10]     = {2, 1, 3, 1, 4, 1, 5, 1, 6, 1};
+      PetscInt    p;
 
-    ierr = DMPlexCreateFromDAG(*dm, 1, numPoints, coneSize, cones, coneOrientations, vertexCoords);CHKERRQ(ierr);
-    for (p = 0; p < 5; ++p) {
-      ierr = DMPlexSetLabelValue(*dm, "marker", markerPoints[p*2], markerPoints[p*2+1]);CHKERRQ(ierr);
+      ierr = DMPlexCreateFromDAG(*dm, 1, numPoints, coneSize, cones, coneOrientations, vertexCoords);CHKERRQ(ierr);
+      for (p = 0; p < 5; ++p) {
+        ierr = DMPlexSetLabelValue(*dm, "marker", markerPoints[p*2], markerPoints[p*2+1]);CHKERRQ(ierr);
+      }
     }
-  }
-  if (interpolate) {
-    DM idm;
-    ierr = DMPlexInterpolate(*dm, &idm);CHKERRQ(ierr);
-    ierr = DMPlexCopyCoordinates(*dm, idm);CHKERRQ(ierr);
-    ierr = DMPlexCopyLabels(*dm, idm);CHKERRQ(ierr);
-    ierr = DMDestroy(dm);CHKERRQ(ierr);
-    *dm = idm;
+    if (interpolate) {
+      DM idm;
+      ierr = DMPlexInterpolate(*dm, &idm);CHKERRQ(ierr);
+      ierr = DMPlexCopyCoordinates(*dm, idm);CHKERRQ(ierr);
+      ierr = DMPlexCopyLabels(*dm, idm);CHKERRQ(ierr);
+      ierr = DMDestroy(dm);CHKERRQ(ierr);
+      *dm = idm;
+    }
   }
   ierr = PetscObjectSetName((PetscObject) *dm, "Hybrid Mesh");CHKERRQ(ierr);
   ierr = DMViewFromOptions(*dm, NULL, "-dm_view");CHKERRQ(ierr);
