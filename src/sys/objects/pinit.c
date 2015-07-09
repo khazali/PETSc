25e0d34a49aaa83d97b4cc44406f53453e750bb1
John O'Sullivan <josullivan1@luc.edu>
2015-07-09 11:47:56 -0500

    Removed .idea

--- src/sys/objects/pinit.c
+++ src/sys/objects/pinit.c
@@ -551,6 +551,9 @@ PetscErrorCode  PetscInitializeSAWs(const char help[])
         ierr = PetscStrreplace(PETSC_COMM_WORLD,"${PETSC_DIR}/share/petsc/saws",root,PETSC_MAX_PATH_LEN);CHKERRQ(ierr);
         PetscStackCallSAWs(SAWs_Set_Document_Root,(root));CHKERRQ(ierr);
       }
+
+
+
     }
     ierr = PetscOptionsHasName(NULL,"-saws_local",&flg2);CHKERRQ(ierr);
     if (flg2) {
