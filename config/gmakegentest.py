0693fc4e6b24d5422441d4bc43f2513889687ed9
Scott Kruger <kruger@txcorp.com>
2016-10-10 13:36:29 -0600

    Further progress on moving to new test system
    0. Bug fixes that Barry reported
    1. Further development of determining whether tests are run
    2. Write out scripts in the PETSC_ARCH/tests subdirectories
    3. Other miscellaneous improvements as development progressed

--- config/gmakegentest.py
+++ config/gmakegentest.py
@@ -10,8 +10,8 @@ from cmakegen import defaultdict # collections.defaultdict, with fallback for py
 from gmakegen import *
 
 import inspect
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-maintdir=os.path.join(os.path.join(os.path.dirname(currentdir),'bin'),'maint')
+thisscriptdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+maintdir=os.path.join(os.path.join(os.path.dirname(thisscriptdir),'bin'),'maint')
 sys.path.insert(0,maintdir) 
 from examplesWalker import *
 
@@ -30,7 +30,21 @@ class generateExamples(Petsc,PETScExamples):
 
     # Language for requirements
     self.precision_types="single double quad int32".split()
+    self.integer_types="int32 int64".split()
     self.languages="fortran cuda cxx".split()    # Always requires C so do not list
+
+    # Save the examplate template in a string
+    template_name=os.path.join(thisscriptdir,"example_template.sh.in")
+    fh=open(template_name,"r")
+    self.templateStr=fh.read()
+    fh.close
+
+    # Adding a dictionary for storing sources
+    self.sources={}
+    for pkg in PKGS:
+      self.sources[pkg]=[]
+
+    self.ptNaming=True
     return
 
   def parseExampleFile(self,srcfile,basedir,srcDict):
@@ -52,11 +66,13 @@ class generateExamples(Petsc,PETScExamples):
     fsplit=fileStr.split("/*TEST")[1:]
     if len(fsplit)==0: return False
     srcTests=[]
-    for t in fsplit[1:]: 
-      srcTests.append(t.split("TEST*/")[0].strip())
+    for t in fsplit: srcTests.append(t.split("TEST*/")[0].strip())
 
     # Now take the strings and put them into a dictionary
     for test in srcTests:
+
+      # Required strings: script OR nsize
+      foundSize=False; foundScript=False
 
       # The dictionary key is determined by output_suffix.
       # Allow anywhere in the file so need to grab it first
@@ -68,13 +84,26 @@ class generateExamples(Petsc,PETScExamples):
         if var=="output_suffix":
           if len(val)>0:
             testname=testname+"_"+val
+        if var=="nsize": foundSize=True
+        if var=="script": foundScript=True
 
+      #print "1> ", self.nameSpace(srcfile,basedir), testname
       # If folks keep forgetting to put output_suffix, problems
+      rpath=self.relpath(curdir, basedir)
       if srcDict.has_key(testname):
-        print "Duplicate test name detected in file "+srcfile+" in "+basedir
-        print "  Check output_suffix in tests"
+        print "Duplicate test in file "+srcfile+" in "+rpath +". Check output_suffix."
         continue
+
+      # Make sure it passes basic requirements
+      if not (foundSize or foundScript):
+        print "Test with insufficient info in file "+srcfile+" in "+rpath +". Check nsize."
+        continue
+
       srcDict[testname]={}
+      if foundSize: 
+        srcDict[testname]['abstracted']=True
+      else:
+        srcDict[testname]['abstracted']=False
       
       i=-1
       for line in test.split("\n"):
@@ -83,6 +112,7 @@ class generateExamples(Petsc,PETScExamples):
         var=line.split(":")[0].strip()
         val=line.split(":")[1].strip()
         # requires is comma delimited list so make list
+        if var=="nsize": val=int(val) 
         if var=="requires": 
           val=val.split(",")
           if langReq: val.append(langReq)
@@ -96,10 +126,59 @@ class generateExamples(Petsc,PETScExamples):
     os.chdir(curdir)
     return True
 
-  def  genRunScript(self,exfile,root,srcDict):
+  def addToSources(self,exfile,root):
     """
-      Generate the bash script
+      Put into data structure that allows easy generation of makefile
+      Note that our testfiles are not separated by language
     """
+    pkg=self.relpath(self.petsc_dir,root).split("/")[1]
+    self.sources[pkg].append(exfile)
+    return
+
+  def getExecname(self,exfile,root):
+    """
+      Generate bash script using template found next to this file.  
+      This file is read in at constructor time to avoid file I/O
+    """
+    rpath=self.relpath(self.petsc_dir,root)
+    if self.single_ex:
+      execname=rpath.split("/")[1]+"-ex"
+    else:
+      execname=os.path.splitext(exfile)[0]
+    return execname
+
+
+  def genRunScript(self,testname,root,testDict):
+    """
+      Generate bash script using template found next to this file.  
+      This file is read in at constructor time to avoid file I/O
+    """
+    # runscript_dir directory has to be consistent with gmakefile
+    rpath=self.relpath(self.petsc_dir,root)
+    runscript_dir=os.path.join(self.petsc_dir,self.petsc_arch,"tests",rpath.lstrip("src/"))
+    if not os.path.isdir(runscript_dir): os.makedirs(runscript_dir)
+    fh=open(os.path.join(runscript_dir,testname+".sh"),"w")
+    
+    if testDict['abstracted']:
+      # Setup the variables in template_string that need to be substituted
+      subst={}
+      subst['MPIEXEC']=self.conf['MPIEXEC']
+      subst['MPIARGSIZE']="-n "+str(testDict['nsize'])
+      subst['EXEC']=testDict['execname']
+      subst['ARGS']=(testDict['args'] if testDict.has_key('args') else " ")
+      subst['TESTNAME']=testname
+      subst['DIFF']=self.conf['DIFF']
+      subst['RM']=self.conf['RM']
+      testStr=self.templateStr
+      for subkey in subst:
+        patt="r\${"+subkey+"}"
+        testStr=re.sub(patt,subst[subkey],testStr)
+      fh.write(testStr+"\n")
+    else:
+      fh.write(testDict['script']+"\n")
+
+    fh.close()
+    os.chmod(os.path.join(runscript_dir,testname+".sh"),0777)
     return
 
   def  genScriptsAndInfo(self,exfile,root,srcDict):
@@ -110,24 +189,38 @@ class generateExamples(Petsc,PETScExamples):
       3. Generate the data needed to write out the makefile in a
          convenient way
     """
+    debug=False
+    fileIsTested=False
+    execname=self.getExecname(exfile,root)
     for test in srcDict:
-      isrun=self.determineIfRun(test,srcDict[test])
+      srcDict[test]['execname']=execname  # Convenience
+      isrun=self.determineIfRun(srcDict[test])
       if isrun:
+        fileIsTested=True
         self.genRunScript(test,root,srcDict[test])
+      if debug: print self.nameSpace(exfile,root), test, isrun
+    if fileIsTested:
+      self.addToSources(exfile,root)
     return
 
-  def determineIfRun(self,testName,testDict):
+  def determineIfRun(self,testDict):
     """
     Based on the requirements listed in the src file and the petscconf.h
     info, determine whether this test should be run or not.
     """
     isrun=True
+    indent="  "
+    debug=False
+
     # MPI requirements
     if testDict.has_key('nsize'):
-      if testDict[nsize]>1 and self.conf['MPI_IS_MPIUNI']==1: return False
+      if testDict['nsize']>1 and self.conf['MPI_IS_MPIUNI']==1: 
+        if debug: print indent+"Cannot run parallel tests"
+        return False
  
     if testDict.has_key('requires'):
       for requirement in testDict['requires']:
+        if debug: print indent+"Requirement: ", requirement
         isNull=False
         if requirement.startswith("!"):
           requirement=requirement[1:]; isNull=True
@@ -141,18 +234,42 @@ class generateExamples(Petsc,PETScExamples):
             if isNull: return False
           else:
             return False
-        # Precision requirement
-        # TODO: int32 is special -- need to figure out
+        # Precision requirement for reals
         if requirement in self.precision_types:
           if self.conf['PETSC_PRECISION']==requirement:
             if isNull: return False
           else:
             return False
+        # Precision requirement for ints
+        if requirement in self.integer_types:
+          if requirement=="int32":
+            if self.conf['PETSC_SIZEOF_INT']==4:
+              if isNull: return False
+            else:
+              return False
+          if requirement=="int64":
+            if self.conf['PETSC_SIZEOF_INT']==8:
+              if isNull: return False
+            else:
+              return False
         # Defines
         if "define(" in requirement:
-          pass
-        # Rest should be packages
+          reqdef=requirement.split("(")[1].split(")")[0]
+          val=(reqdef.split()[1] if " " in reqdef else "")
+          if self.conf.has_key(reqdef):
+            if val:
+              if self.conf[reqdef]==val:
+                if isNull: return False
+              else:
+                return False
+            else:
+              if isNull: return False
+          else:
+            return False
 
+        # Rest should be packages that we can just get from conf
+        if self.conf.get(requirement): 
+          if isNull: return False
 
     return isrun
 
