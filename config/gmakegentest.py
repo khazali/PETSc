019f0c20dde1261507ec15a150e78623d12be2cf
Scott Kruger <kruger@txcorp.com>
2016-11-15 17:28:26 -0700

    Bug fix

--- config/gmakegentest.py
+++ config/gmakegentest.py
@@ -569,15 +569,15 @@ class generateExamples(Petsc,PETScExamples):
     """
      Overwrite of the method in the base PETSc class 
     """
-      def write(stem, srcs):
-          fd.write('%s :=\n' % stem)
-          for lang in LANGS:
-              fd.write('%(stem)s.%(lang)s := %(srcs)s\n' % dict(stem=stem, lang=lang, srcs=' '.join(srcs[lang])))
-              fd.write('%(stem)s += $(%(stem)s.%(lang)s)\n' % dict(stem=stem, lang=lang))
-      for pkg in PKGS:
-          srcs = self.gen_pkg(pkg)
-          write(prefix + pkg, srcs)
-      return self.gendeps
+    def write(stem, srcs):
+        fd.write('%s :=\n' % stem)
+        for lang in LANGS:
+            fd.write('%(stem)s.%(lang)s := %(srcs)s\n' % dict(stem=stem, lang=lang, srcs=' '.join(srcs[lang])))
+            fd.write('%(stem)s += $(%(stem)s.%(lang)s)\n' % dict(stem=stem, lang=lang))
+    for pkg in PKGS:
+        srcs = self.gen_pkg(pkg)
+        write(prefix + pkg, srcs)
+    return self.gendeps
 
   def gen_pkg(self, pkg):
     """
