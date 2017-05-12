73fa3f6737d6e25b9de7020f91eb151abe457bd9
Emil Constantinescu <emconsta@mcs.anl.gov>
2017-05-12 11:37:42 -0500

    Making the plots prettier

--- src/ts/examples/tutorials/spectral-toy/adjointreads.m
+++ src/ts/examples/tutorials/spectral-toy/adjointreads.m
@@ -1,17 +1,28 @@
 clear all;
 close all;
 
+
+figure(1);set(gca,'FontSize',18);
 run('IC_OBJ.m')
-plot(xg,ic,'k-');
+plot(xg,ic,'k-','LineWidth',2);
 hold on
-plot(xg,obj,'r');
+plot(xg,obj,'r-','LineWidth',2);
 
 run('optimize00.m')
-plot(xg,Init_ts,'k*-');
+plot(xg,Init_ts,'k*','Markersize',14);
 run('optimize09.m')
-plot(xg,Init_ts,'ro-');
+plot(xg,Init_ts,'ro','Markersize',14);
 
 xlabel('x (GLL grid)');
 ylabel('f(x)- objective');
 
-legend('IC','OBJ','Iter 1','Iter 9')
+legend('IC','OBJ','Iter 0','Iter 9')
+
+
+figure(2);set(gca,'FontSize',18);hold on
+
+run('optimize00.m')
+plot(xg,Grad,'k*-');
+run('optimize09.m')
+plot(xg,Grad,'ro-');
+
