7e574d80abf02a912ebea337ecfa01fc44c14e93
Oana Marin <oanam@anl.gov>
2017-05-12 14:34:47 -0500

    Associated matlab script, currently not so neat solution (writing .m files instead of ASCII/bin)

--- src/ts/examples/tutorials/spectral-toy/adjointreads.m
+++ src/ts/examples/tutorials/spectral-toy/adjointreads.m
@@ -1,16 +1,15 @@
 clear all;
 close all;
 
-
 figure(1);set(gca,'FontSize',18);
 run('IC_OBJ.m')
 plot(xg,ic,'k-','LineWidth',2);
 hold on
 plot(xg,obj,'r-','LineWidth',2);
 
-run('optimize00.m')
+run('PDEadjoint/optimize00.m')
 plot(xg,Init_ts,'k*','Markersize',14);
-run('optimize09.m')
+run('PDEadjoint/optimize09.m')
 plot(xg,Init_ts,'ro','Markersize',14);
 
 xlabel('x (GLL grid)');
@@ -18,11 +17,14 @@ ylabel('f(x)- objective');
 
 legend('IC','OBJ','Iter 0','Iter 9')
 
-
 figure(2);set(gca,'FontSize',18);hold on
 
-run('optimize00.m')
+run('PDEadjoint/optimize00.m')
 plot(xg,Grad,'k*-');
-run('optimize09.m')
+run('PDEadjoint/optimize09.m')
 plot(xg,Grad,'ro-');
+xlabel('x (GLL grid)');
+ylabel('f(x)- objective');
+
+legend('Grad at it=0','Grad at it=9')
 
