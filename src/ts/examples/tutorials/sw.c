d7a67699e3d6ef11526174ee2c7fad916c4df0fd
Hong Zhang <hongzhang@anl.gov>
2017-07-29 17:41:01 -0500

    Fix wrong Jacobian
    For the u equation, the derivative wrt to uw got wrong sign. For the h equation, the derivative wrt to vc was missing. Now we have perfect Jacobian!

--- src/ts/examples/tutorials/sw.c
+++ src/ts/examples/tutorials/sw.c
@@ -98,17 +98,17 @@ PetscErrorCode RHSFunction(TS ts,PetscReal ftime,Vec U,Vec F,void *ptr)
      Place a solid wall at north and south boundaries, velocities are reflected,e.g. v(-1) = v(+1)
      Height is assumed to be constant, e.g. h(-1) = h(0)
   */
-  if (ys == 0) { /* south pole */
-    for (i=xs; i<xs+xm; i++) {
-      uarr[ys-1][i].u = -uarr[ys+1][i].u;
-      uarr[ys-1][i].v = -uarr[ys+1][i].v;
-      uarr[ys-1][i].h = uarr[ys][i].h;
+  if (ys == 0) {
+    for (i=xs-2; i<xs+xm+2; i++) {
+      uarr[-1][i].u = -uarr[0][i].u;
+      uarr[-1][i].v = -uarr[0][i].v;
+      uarr[-1][i].h = uarr[0][i].h;
     }
   }
-  if (ys+ym == My) { /* north pole */
-    for (i=xs; i<xs+xm; i++) {
-      uarr[My][i].u = -uarr[My-2][i].u;
-      uarr[My][i].v = -uarr[My-2][i].v;
+  if (ys+ym == My) {
+    for (i=xs-2; i<xs+xm+2; i++) {
+      uarr[My][i].u = -uarr[My-1][i].u;
+      uarr[My][i].v = -uarr[My-1][i].v;
       uarr[My][i].h = uarr[My-1][i].h;
     }
   }
@@ -175,7 +175,6 @@ PetscErrorCode RHSFunction(TS ts,PetscReal ftime,Vec U,Vec F,void *ptr)
                      );
     }
   }
-
   /*
      Restore vectors
   */
@@ -185,16 +184,16 @@ PetscErrorCode RHSFunction(TS ts,PetscReal ftime,Vec U,Vec F,void *ptr)
   PetscFunctionReturn(0);
 }
 
-PetscErrorCode RHSJacobian(TS ts,PetscReal t,Vec U,Mat A,Mat BB,void *ctx)
+PetscErrorCode RHSJacobian(TS ts,PetscReal t,Vec U,Mat A,Mat BB,void *ptx)
 {
-  Model_SW       *sw=(Model_SW*)ctx;
+  Model_SW       *sw=(Model_SW*)ptx;
   DM             da;
   PetscInt       i,j,Mx,My,xs,ys,xm,ym,ph,qh;
   PetscReal      a,g,alpha,omega,lat,dlat,dlon;
   Field          **uarr;
   Vec            localU;
-  MatStencil     stencil[18],rowstencil;
-  PetscScalar    entries[18];
+  MatStencil     stencil[19],rowstencil;
+  PetscScalar    entries[19];
   PetscErrorCode ierr;
 
   PetscFunctionBegin;
@@ -224,7 +223,7 @@ PetscErrorCode RHSJacobian(TS ts,PetscReal t,Vec U,Mat A,Mat BB,void *ctx)
   /*
      Get pointers to vector data
   */
-  ierr = DMDAVecGetArrayRead(da,localU,&uarr);CHKERRQ(ierr);
+  ierr = DMDAVecGetArray(da,localU,&uarr);CHKERRQ(ierr);
 
   /*
      Get local grid boundaries
@@ -232,24 +231,25 @@ PetscErrorCode RHSJacobian(TS ts,PetscReal t,Vec U,Mat A,Mat BB,void *ctx)
   ierr = DMDAGetCorners(da,&xs,&ys,NULL,&xm,&ym,NULL);CHKERRQ(ierr);
 
   /*
-     Place a solid wall at boundaries, velocities are reflected
+     Place a solid wall at north and south boundaries, velocities are reflected,e.g. v(-1) = v(+1)
+     Height is assumed to be constant, e.g. h(-1) = h(0)
   */
-  if (ys == 0) { /* south pole */
-    for (i=xs; i<xs+xm; i++) {
-      uarr[ys-1][i].u = -uarr[ys+1][i].u;
-      uarr[ys-1][i].v = -uarr[ys+1][i].v;
-      uarr[ys-1][i].h = uarr[ys][i].h;
+  if (ys == 0) {
+    for (i=xs-2; i<xs+xm+2; i++) {
+      uarr[-1][i].u = -uarr[0][i].u;
+      uarr[-1][i].v = -uarr[0][i].v;
+      uarr[-1][i].h = uarr[0][i].h;
     }
   }
-  if (ys+ym == My) { /* north pole */
-    for (i=xs; i<xs+xm; i++) {
-      uarr[My][i].u = -uarr[My-2][i].u;
-      uarr[My][i].v = -uarr[My-2][i].v;
+  if (ys+ym == My) {
+    for (i=xs-2; i<xs+xm+2; i++) {
+      uarr[My][i].u = -uarr[My-1][i].u;
+      uarr[My][i].v = -uarr[My-1][i].v;
       uarr[My][i].h = uarr[My-1][i].h;
     }
   }
 
-  for (i=0; i<18; i++) stencil[i].k = 0;
+  for (i=0; i<19; i++) stencil[i].k = 0;
   rowstencil.k = 0;
   rowstencil.c = 0;
   for (j=ys; j<ys+ym; j++) {
@@ -271,8 +271,8 @@ PetscErrorCode RHSJacobian(TS ts,PetscReal t,Vec U,Mat A,Mat BB,void *ctx)
     stencil[11].j = j;
 
     /* Relocate the ghost points at north and south boundaries */
-    if (j==0) stencil[6].j = j+1;
-    if (j==My-1) stencil[0].j = j-1;
+    if (j==0) stencil[6].j = j;
+    if (j==My-1) stencil[0].j = j;
 
     rowstencil.j = j;
     for (i=xs; i<xs+xm; i++) {
@@ -288,24 +288,25 @@ PetscErrorCode RHSJacobian(TS ts,PetscReal t,Vec U,Mat A,Mat BB,void *ctx)
       vep = uarr[j][i+ph].v;
       vwp = uarr[j][i-ph].v;
 
-      stencil[0].i = i; stencil[0].c = 0; entries[0] = -1./(2.*a*dlat)*vc; /* un */
-      stencil[1].i = i-2; stencil[1].c = 0; entries[1] = alpha/2./a*PetscTanReal(lat)*vwp; /* uwp */
-      stencil[2].i = i-1; stencil[2].c = 0; entries[2] = -1./(2.*a*dlat)*uc/PetscCosReal(lat); /* uw */
-      stencil[3].i = i; stencil[3].c = 0; entries[3] = -1./(2.*a*dlat)/PetscCosReal(lat)*(ue-uw)+(1.-alpha)/a*PetscTanReal(lat)*vc; /* uc */
-      stencil[4].i = i+1; stencil[4].c = 0; entries[4] = -entries[2]; /* ue */
-      stencil[5].i = i+2; stencil[5].c = 0; entries[5] = alpha/2./a*PetscTanReal(lat)*vep; /* uep */
-      stencil[6].i = i; stencil[6].c = 0; entries[6] = -entries[0]; /* us */
-      stencil[7].i = i-2; stencil[7].c = 1; entries[7] = alpha/2.*(fc+uwp/a*PetscTanReal(lat)); /* vwp */
-      stencil[8].i = i; stencil[8].c = 1; entries[8] = -1./(2.*a*dlat)*(un-us)+(1.-alpha)*(fc+uc/a*PetscTanReal(lat)); /* vc */
-      stencil[9].i = i+2; stencil[9].c = 1; entries[9] = alpha/2.*(fc+uep/a*PetscTanReal(lat)); /* vep */
-      stencil[10].i = i-2; stencil[10].c = 2; entries[10] =  1./(2.*a*dlat)*2.*g/(sw->p*PetscCosReal(lat));/* hwp */
-      stencil[11].i = i+2; stencil[11].c = 2; entries[11] = -entries[10]; /* hep */
+      stencil[0].i  = i;    stencil[0].c  = 0; entries[0]  = -1./(2.*a*dlat)*vc; /* un */
+      stencil[1].i  = i-ph; stencil[1].c  = 0; entries[1]  = alpha/2./a*PetscTanReal(lat)*vwp; /* uwp */
+      stencil[2].i  = i-1;  stencil[2].c  = 0; entries[2]  = 1./(2.*a*dlat)*uc/PetscCosReal(lat); /* uw */
+      stencil[3].i  = i;    stencil[3].c  = 0; entries[3]  = -1./(2.*a*dlat)/PetscCosReal(lat)*(ue-uw)+(1.-alpha)/a*PetscTanReal(lat)*vc; /* uc */
+      stencil[4].i  = i+1;  stencil[4].c  = 0; entries[4]  = -entries[2]; /* ue */
+      stencil[5].i  = i+ph; stencil[5].c  = 0; entries[5]  = alpha/2./a*PetscTanReal(lat)*vep; /* uep */
+      stencil[6].i  = i;    stencil[6].c  = 0; entries[6]  = -entries[0]; /* us */
+      stencil[7].i  = i-ph; stencil[7].c  = 1; entries[7]  = alpha/2.*(fc+uwp/a*PetscTanReal(lat)); /* vwp */
+      stencil[8].i  = i;    stencil[8].c  = 1; entries[8]  = -1./(2.*a*dlat)*(un-us)+(1.-alpha)*(fc+uc/a*PetscTanReal(lat)); /* vc */
+      stencil[9].i  = i+ph; stencil[9].c  = 1; entries[9]  = alpha/2.*(fc+uep/a*PetscTanReal(lat)); /* vep */
+      stencil[10].i = i-ph; stencil[10].c = 2; entries[10] = 1./(2.*a*dlat)*2.*g/(sw->p*PetscCosReal(lat));/* hwp */
+      stencil[11].i = i+ph; stencil[11].c = 2; entries[11] = -entries[10]; /* hep */
 
       /* flip the sign */
       if (j==0) entries[6] = -entries[6];
       if (j==My-1) entries[0] = -entries[0];
 
       rowstencil.i = i;
+      //for (int k=0;k<19;k++) entries[k] += 30000+10*k;
       ierr = MatSetValuesStencil(A,1,&rowstencil,12,stencil,entries,ADD_VALUES);CHKERRQ(ierr);
     }
   }
@@ -326,18 +327,18 @@ PetscErrorCode RHSJacobian(TS ts,PetscReal t,Vec U,Mat A,Mat BB,void *ctx)
     stencil[5].j = j;
     stencil[6].j = j;
     stencil[7].j = j-1;
-    stencil[8].j = j+1;
-    stencil[9].j = j-1;
+    stencil[8].j = j+qh;
+    stencil[9].j = j-qh;
 
     /* Relocate the ghost points at north and south boundaries */
     if (j == 0) {
-      stencil[2].j = j+1;
-      stencil[7].j = j+1;
+      stencil[2].j = j;
+      stencil[7].j = j;
       stencil[9].j = j;
     }
     if (j == My-1) {
-      stencil[0].j = j-1;
-      stencil[3].j = j-1;
+      stencil[0].j = j;
+      stencil[3].j = j;
       stencil[8].j = j;
     }
 
@@ -353,16 +354,16 @@ PetscErrorCode RHSJacobian(TS ts,PetscReal t,Vec U,Mat A,Mat BB,void *ctx)
       vn  = uarr[j+1][i].v;
       vc  = uarr[j][i].v;
 
-      stencil[0].i = i; stencil[0].c = 0; entries[0] = -alpha/2.*(fnq+2.*unq/a*PetscTanReal(lat+qh*dlat)); /* unq */
-      stencil[1].i = i; stencil[1].c = 0; entries[1] = -1./(2.*a*dlat)/PetscCosReal(lat)*(ve-vw)-(1.-alpha)*(fc+2.*uc/a*PetscTanReal(lat)); /* uc */
-      stencil[2].i = i; stencil[2].c = 0; entries[2] = -alpha/2.*(fsq+2.*usq/a*PetscTanReal(lat-qh*dlat)); /* usq */
-      stencil[3].i = i; stencil[3].c = 1; entries[3] =  -1./(2.*a*dlat)*vc; /* vn */
-      stencil[4].i = i-1; stencil[4].c = 1; entries[4] = 1./(2.*a*dlat)*uc/PetscCosReal(lat); /* vw */
-      stencil[5].i = i; stencil[5].c = 1; entries[5] =  -1./(2.*a*dlat)*(vn-vs); /* vc */
+      stencil[0].i = i;   stencil[0].c = 0; entries[0] = -alpha/2.*(fnq+2.*unq/a*PetscTanReal(lat+qh*dlat)); /* unq */
+      stencil[1].i = i;   stencil[1].c = 0; entries[1] = -1./(2.*a*dlat*PetscCosReal(lat))*(ve-vw)-(1.-alpha)*(fc+2.*uc/a*PetscTanReal(lat)); /* uc */
+      stencil[2].i = i;   stencil[2].c = 0; entries[2] = -alpha/2.*(fsq+2.*usq/a*PetscTanReal(lat-qh*dlat)); /* usq */
+      stencil[3].i = i;   stencil[3].c = 1; entries[3] = -1./(2.*a*dlat)*vc; /* vn */
+      stencil[4].i = i-1; stencil[4].c = 1; entries[4] = 1./(2.*a*dlat*PetscCosReal(lat))*uc; /* vw */
+      stencil[5].i = i;   stencil[5].c = 1; entries[5] = -1./(2.*a*dlat)*(vn-vs); /* vc */
       stencil[6].i = i+1; stencil[6].c = 1; entries[6] = -entries[4]; /* ve */
-      stencil[7].i = i; stencil[7].c = 1; entries[7] = -entries[3]; /* vs */
-      stencil[8].i = i; stencil[8].c = 2; entries[8] = -1./(2.*a*dlat)*2.*g/sw->q; /* hnq */
-      stencil[9].i = i; stencil[9].c = 2; entries[9] = -entries[8]; /* hsq */
+      stencil[7].i = i;   stencil[7].c = 1; entries[7] = -entries[3]; /* vs */
+      stencil[8].i = i;   stencil[8].c = 2; entries[8] = -1./(2.*a*dlat*sw->q)*2.*g; /* hnq */
+      stencil[9].i = i;   stencil[9].c = 2; entries[9] = -entries[8]; /* hsq */
 
       /* flip the sign */
       if (j == 0) {
@@ -373,8 +374,8 @@ PetscErrorCode RHSJacobian(TS ts,PetscReal t,Vec U,Mat A,Mat BB,void *ctx)
         entries[0] = -entries[0];
         entries[3] = -entries[3];
       }
-
       rowstencil.i = i;
+      //for (int k=0;k<19;k++) entries[k] += 50000+100*k;
       ierr = MatSetValuesStencil(A,1,&rowstencil,10,stencil,entries,ADD_VALUES);CHKERRQ(ierr);
     }
   }
@@ -383,46 +384,47 @@ PetscErrorCode RHSJacobian(TS ts,PetscReal t,Vec U,Mat A,Mat BB,void *ctx)
   for (j=ys; j<ys+ym; j++) {
     lat = -PETSC_PI/2.+j*dlat+dlat/2.; /* shift half dlat to avoid singularity */
 
-    stencil[0].j  = j+1;
-    stencil[1].j  = j+1;
+    stencil[0].j  = j+qh;
+    stencil[1].j  = j+qh;
     stencil[2].j  = j;
     stencil[3].j  = j;
     stencil[4].j  = j;
-    stencil[5].j  = j-1;
-    stencil[6].j  = j-1;
-    stencil[7].j  = j+1;
-    stencil[8].j  = j+1;
-    stencil[9].j  = j+1;
-    stencil[10].j = j-1;
-    stencil[11].j = j-1;
-    stencil[12].j = j-1;
-    stencil[13].j = j+1;
-    stencil[14].j = j;
+    stencil[5].j  = j-qh;
+    stencil[6].j  = j-qh;
+    stencil[7].j  = j+qh;
+    stencil[8].j  = j+qh;
+    stencil[9].j  = j+qh;
+    stencil[10].j = j;
+    stencil[11].j = j-qh;
+    stencil[12].j = j-qh;
+    stencil[13].j = j-qh;
+    stencil[14].j = j+1;
     stencil[15].j = j;
     stencil[16].j = j;
-    stencil[17].j = j-1;
+    stencil[17].j = j;
+    stencil[18].j = j-1;
 
     /* Relocate the ghost points at north and south boundaries */
     if (j == 0) {
-      stencil[5].j  = j+1;
-      stencil[6].j  = j+1;
-      stencil[10].j = j+1;
-      stencil[11].j = j+1;
-      stencil[12].j = j+1;
-      stencil[17].j = j;
+      stencil[5].j  = j;
+      stencil[6].j  = j;
+      stencil[11].j = j;
+      stencil[12].j = j;
+      stencil[13].j = j;
+      stencil[18].j = j;
     }
     if (j == My-1) {
-      stencil[0].j  = j-1;
-      stencil[1].j  = j-1;
-      stencil[7].j  = j-1;
-      stencil[8].j  = j-1;
-      stencil[9].j  = j-1;
-      stencil[13].j = j;
+      stencil[0].j  = j;
+      stencil[1].j  = j;
+      stencil[7].j  = j;
+      stencil[8].j  = j;
+      stencil[9].j  = j;
+      stencil[14].j = j;
     }
 
     rowstencil.j  = j;
     for (i=xs; i<xs+xm; i++) {
-      PetscReal uc,uep,uwp,uepnq,uwpnq,uepsq,uwpsq,vc,vnq,vsq,vepnq,vwpnq,vepsq,vwpsq,hc,he,hw;
+      PetscReal uc,uep,uwp,uepnq,uwpnq,uepsq,uwpsq,vc,vnq,vsq,vepnq,vwpnq,vepsq,vwpsq,hc,he,hw,hs,hn;
       uc    = uarr[j][i].u;
       uep   = uarr[j][i+ph].u;
       uwp   = uarr[j][i-ph].u;
@@ -440,33 +442,36 @@ PetscErrorCode RHSJacobian(TS ts,PetscReal t,Vec U,Mat A,Mat BB,void *ctx)
       hc    = uarr[j][i].h;
       he    = uarr[j][i+1].h;
       hw    = uarr[j][i-1].h;
+      hs    = uarr[j-1][i].h;
+      hn    = uarr[j+1][i].h;
 
-      stencil[0].i = i-2; stencil[0].c = 0; entries[0] = 1./(2.*a*dlat)*2.*hc/PetscCosReal(lat)*alpha/2./sw->p; /* uwpnq */
-      stencil[1].i = i+2; stencil[1].c = 0; entries[1] = -entries[0]; /* uepnq */
-      stencil[2].i = i-2; stencil[2].c = 0; entries[2] = 1./(2.*a*dlat)*2.*hc/PetscCosReal(lat)*(1.-alpha)/sw->p; /* uwp */
-      stencil[3].i = i; stencil[3].c = 0; entries[3] =  -1./(2.*a*dlat)/PetscCosReal(lat)*(he-hw); /* uc */
-      stencil[4].i = i+2; stencil[4].c = 0; entries[4] = -entries[2]; /* uep */
-      stencil[5].i = i-2; stencil[5].c = 0; entries[5] = entries[0]; /* uwpsq */
-      stencil[6].i = i+2; stencil[6].c = 0; entries[6] = -entries[0]; /* uepsq */
-      stencil[7].i = i-2; stencil[7].c = 1; entries[7] = -1./(2.*a*dlat)*2.*hc/PetscCosReal(lat)*alpha/2.*PetscCosReal(lat+qh*dlat)/sw->q; /* vwpnq */
-      stencil[8].i = i; stencil[8].c = 1; entries[8] = -1./(2.*a*dlat)*2.*hc/PetscCosReal(lat)*(1.-alpha)*PetscCosReal(lat+qh*dlat)/sw->q; /* vnq */
-      stencil[9].i = i+2; stencil[9].c = 1; entries[9] = entries[7]; /* vepnq */
-      stencil[10].i = i-2; stencil[10].c = 1; entries[10] = 1./(2.*a*dlat)*2.*hc/PetscCosReal(lat)*alpha/2.*PetscCosReal(lat-qh*dlat)/sw->q; /* vwpsq */
-      stencil[11].i = i; stencil[11].c = 1; entries[11] = 1./(2.*a*dlat)*2.*hc/PetscCosReal(lat)*(1.-alpha)*PetscCosReal(lat-qh*dlat)/sw->q; /* vsq */
-      stencil[12].i = i+2; stencil[12].c = 1; entries[12] = entries[10]; /* vepsq */
-      stencil[13].i = i; stencil[13].c = 2; entries[13] = -1./(2.*a*dlat)*vc; /* hn */
-      stencil[14].i = i-1; stencil[14].c = 2; entries[14] = 1./(2.*a*dlat)*uc/PetscCosReal(lat); /* hw */
-      stencil[15].i = i; stencil[15].c = 2; entries[15] = -1./(2.*a*dlat)*(2./PetscCosReal(lat)*((1.-alpha)*(uep-uwp)+alpha/2.*(uepnq-uwpnq+uepsq-uwpsq))/sw->p + 2./PetscCosReal(lat)*((1.-alpha)*(vnq*PetscCosReal(lat+qh*dlat)-vsq*PetscCosReal(lat-qh*dlat))+alpha/2.*(vepnq*PetscCosReal(lat+qh*dlat)-vepsq*PetscCosReal(lat-qh*dlat)+vwpnq*PetscCosReal(lat+qh*dlat)-vwpsq*PetscCosReal(lat-qh*dlat)))/sw->q); /* hc */
-      stencil[16].i = i+1; stencil[16].c = 2; entries[16] = -entries[14]; /* he */
-      stencil[17].i = i; stencil[17].c = 2; entries[17] = -entries[13]; /* hs */
+      stencil[0].i  = i-ph; stencil[0].c  = 0; entries[0]  = 1./(2.*a*dlat*PetscCosReal(lat)*2.*sw->p)*2.*hc*alpha; /* uwpnq */
+      stencil[1].i  = i+ph; stencil[1].c  = 0; entries[1]  = -entries[0]; /* uepnq */
+      stencil[2].i  = i-ph; stencil[2].c  = 0; entries[2]  = 1./(2.*a*dlat*PetscCosReal(lat)*sw->p)*2.*hc*(1.-alpha); /* uwp */
+      stencil[3].i  = i;    stencil[3].c  = 0; entries[3]  = -1./(2.*a*dlat*PetscCosReal(lat))*(he-hw); /* uc */
+      stencil[4].i  = i+ph; stencil[4].c  = 0; entries[4]  = -entries[2]; /* uep */
+      stencil[5].i  = i-ph; stencil[5].c  = 0; entries[5]  = entries[0]; /* uwpsq */
+      stencil[6].i  = i+ph; stencil[6].c  = 0; entries[6]  = -entries[0]; /* uepsq */
+      stencil[7].i  = i-ph; stencil[7].c  = 1; entries[7]  = -1./(2.*a*dlat*PetscCosReal(lat)*2.*sw->q)*2.*hc*alpha*PetscCosReal(lat+qh*dlat); /* vwpnq */
+      stencil[8].i  = i;    stencil[8].c  = 1; entries[8]  = -1./(2.*a*dlat*PetscCosReal(lat)*sw->q)*2.*hc*(1.-alpha)*PetscCosReal(lat+qh*dlat); /* vnq */
+      stencil[9].i  = i+ph; stencil[9].c  = 1; entries[9]  = entries[7]; /* vepnq */
+      stencil[10].i = i;    stencil[10].c = 1; entries[10] = -1./(2.*a*dlat)*(hn-hs); /* vc */
+      stencil[11].i = i-ph; stencil[11].c = 1; entries[11] = 1./(2.*a*dlat*PetscCosReal(lat)*2.*sw->q)*(2.*hc*alpha*PetscCosReal(lat-qh*dlat)); /* vwpsq */
+      stencil[12].i = i;    stencil[12].c = 1; entries[12] = 1./(2.*a*dlat*PetscCosReal(lat)*sw->q)*2.*hc*(1.-alpha)*PetscCosReal(lat-qh*dlat); /* vsq */
+      stencil[13].i = i+ph; stencil[13].c = 1; entries[13] = entries[11]; /* vepsq */
+      stencil[14].i = i;    stencil[14].c = 2; entries[14] = -1./(2.*a*dlat)*vc; /* hn */
+      stencil[15].i = i-1;  stencil[15].c = 2; entries[15] = 1./(2.*a*dlat*PetscCosReal(lat))*uc; /* hw */
+      stencil[16].i = i;    stencil[16].c = 2; entries[16] = -1./(2.*a*dlat)*(2./PetscCosReal(lat)*((1.-alpha)*(uep-uwp)+alpha/2.*(uepnq-uwpnq+uepsq-uwpsq))/sw->p + 2./PetscCosReal(lat)*((1.-alpha)*(vnq*PetscCosReal(lat+qh*dlat)-vsq*PetscCosReal(lat-qh*dlat))+alpha/2.*(vepnq*PetscCosReal(lat+qh*dlat)-vepsq*PetscCosReal(lat-qh*dlat)+vwpnq*PetscCosReal(lat+qh*dlat)-vwpsq*PetscCosReal(lat-qh*dlat)))/sw->q); /* hc */
+      stencil[17].i = i+1;  stencil[17].c = 2; entries[17] = -entries[15]; /* he */
+      stencil[18].i = i;    stencil[18].c = 2; entries[18] = -entries[14]; /* hs */
 
       /* flip the sign */
       if (j == 0) {
-        entries[5] = -entries[5];
-        entries[6] = -entries[6];
-        entries[10] = -entries[10];
+        entries[5]  = -entries[5];
+        entries[6]  = -entries[6];
         entries[11] = -entries[11];
         entries[12] = -entries[12];
+        entries[13] = -entries[13];
       }
       if (j == My-1) {
         entries[0] = -entries[0];
@@ -475,16 +480,16 @@ PetscErrorCode RHSJacobian(TS ts,PetscReal t,Vec U,Mat A,Mat BB,void *ctx)
         entries[8] = -entries[8];
         entries[9] = -entries[9];
       }
-
       rowstencil.i = i;
-      ierr = MatSetValuesStencil(A,1,&rowstencil,18,stencil,entries,ADD_VALUES);CHKERRQ(ierr);
+      //for (int k=0;k<19;k++) entries[k] += 70000+1000*k;
+      ierr = MatSetValuesStencil(A,1,&rowstencil,19,stencil,entries,ADD_VALUES);CHKERRQ(ierr);
     }
   }
 
   /*
      Restore vectors
   */
-  ierr = DMDAVecRestoreArrayRead(da,localU,&uarr);CHKERRQ(ierr);
+  ierr = DMDAVecRestoreArray(da,localU,&uarr);CHKERRQ(ierr);
   ierr = DMRestoreLocalVector(da,&localU);CHKERRQ(ierr);
   ierr = MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
   ierr = MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
@@ -509,7 +514,7 @@ int main(int argc,char **argv)
   sw.p            = 4;
   sw.q            = 2;
 
-  ierr = DMDACreate2d(PETSC_COMM_WORLD,DM_BOUNDARY_PERIODIC,DM_BOUNDARY_GHOSTED,DMDA_STENCIL_BOX,150,75,PETSC_DECIDE,PETSC_DECIDE,3,sw.p/2,NULL,NULL,&da);CHKERRQ(ierr);
+  ierr = DMDACreate2d(PETSC_COMM_WORLD,DM_BOUNDARY_PERIODIC,DM_BOUNDARY_GHOSTED,DMDA_STENCIL_BOX,150,75,PETSC_DECIDE,PETSC_DECIDE,3,2,NULL,NULL,&da);CHKERRQ(ierr);
   ierr = DMSetFromOptions(da);CHKERRQ(ierr);
   ierr = DMSetUp(da);CHKERRQ(ierr);
   ierr = DMDASetFieldName(da,0,"u");CHKERRQ(ierr);
