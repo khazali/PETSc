e3cd6847df8a778adf940acfdae5bcd7c6356f5c
Mark Adams <mfadams@lbl.gov>
2017-09-07 08:02:59 -0400

    fix error

--- include/petsc/private/dmpicellimpl.h
+++ include/petsc/private/dmpicellimpl.h
@@ -9,7 +9,6 @@
 #include <petscsf.h>
 #include <petsc/private/dmimpl.h>
 #include <petsc/private/isimpl.h>     /* for inline access to atlasOff */
-#include <../src/sys/utils/hash.h>
 
 PETSC_EXTERN PetscLogEvent DMPICell_Solve, DMPICell_SetUp, DMPICell_AddSource, DMPICell_LocateProcess, DMPICell_GetJet, DMPICell_Add1, DMPICell_Add2, DMPICell_Add3;
 
