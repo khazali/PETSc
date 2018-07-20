20c7d2261a2a922a562e2e19509d0ca2952e45dc
Matthew G. Knepley <knepley@gmail.com>
2018-07-20 16:41:38 -0400

    SNES ex4: Now solving the true nonlinear problem - The entropy viscosity will not work until we get second derivatives of shape functions implemented

--- src/snes/examples/tutorials/ex4.c
+++ src/snes/examples/tutorials/ex4.c
@@ -51,18 +51,6 @@ T*/
     value problem to obtain a nonlinear system of equations.  Entropy
     viscosity is used to stabilize the divergence (convective) terms.
 
-\alpha = 1 or 2
-\bata = 0.03
-k = \frac{1}{4} \frac{h_K}{\|\|u\|\|_{\infty(K)}}
-c_R = 2^\frac{4 - 2\alpha}{d}
-var(T) = max_\Omega T - min_\Omega T
-diam(\Omega) = 2^{1/d} for the unit cube
-c(u,T) = c_R \|\|u\|\|_{\infty(\Omega)} var(T) |diam(\Omega)|^{\alpha−2}
-
-\nu_\alpha(T)_K = \beta \|\|u\|\|_{\infty(K)} \mathrm{min} \left( h_K, h^\alpha_K \frac{\|\|R_\alpha(T)\|\|_{\infty(K)}}{c(u,T)} \right)
-
-  The entropy viscosity will be $P_0$ auxiliary field which we update at each SNES iteration.
-
   ------------------------------------------------------------------------F*/
 
 /*
@@ -84,12 +72,17 @@ typedef struct {
   PetscReal prandtl;     /* Prandtl number, ratio of momentum diffusivity to thermal diffusivity */
   PetscReal grashof;     /* Grashof number, ratio of bouyancy to viscous forces */
   PetscReal lidvelocity; /* Shear velocity of top boundary */
+
+  /* https://www.dealii.org/8.5.1/doxygen/deal.II/step_31.html#Stabilizationweakformandspacediscretizationforthetemperatureequation */
+  PetscReal alpha;       /* Stabilization exponent, unitless */
+  PetscReal beta;        /* Stabilization constant, unitless */
 } Parameter;
 
 typedef struct {
   PetscInt  dim;            /* Topological dimension */
   char      filename[2048]; /* The optional mesh file */
   PetscBool simplex;        /* Simplicial mesh */
+  PetscInt  cells[3];       /* The initial domain division */
   PetscBag  params;         /* Problem parameters */
   PetscInt  mms;            /* Number of the MMS solution, or -1 */
 } AppCtx;
@@ -97,12 +90,6 @@ typedef struct {
 static PetscErrorCode coordX(PetscInt dim, PetscReal time, const PetscReal x[], PetscInt Nc, PetscScalar *u, void *ctx)
 {
   u[0] = x[0];
-  return 0;
-}
-
-static PetscErrorCode zero(PetscInt dim, PetscReal time, const PetscReal x[], PetscInt Nc, PetscScalar *u, void *ctx)
-{
-  u[0] = 0.0;
   return 0;
 }
 
@@ -260,7 +247,7 @@ static void f1_T(PetscInt dim, PetscInt Nf, PetscInt NfAux,
                  PetscReal t, const PetscReal x[], PetscInt numConstants, const PetscScalar constants[], PetscScalar f1[])
 {
   PetscInt d;
-  for (d = 0; d < dim; ++d) f1[d] = (1.0 + a[0])*u_x[uOff_x[2]+d];
+  for (d = 0; d < dim; ++d) f1[d] = (1.0 + a[1])*u_x[uOff_x[2]+d];
 }
 
 /* Pr (U . grad T) - PR (x^2 + y^2) */
@@ -276,18 +263,23 @@ static void f0_mms_0_T(PetscInt dim, PetscInt Nf, PetscInt NfAux,
 
 static PetscErrorCode ProcessOptions(MPI_Comm comm, AppCtx *options)
 {
+  PetscInt       n = 3;
   PetscErrorCode ierr;
 
   PetscFunctionBeginUser;
   options->dim         = 2;
   options->filename[0] = '\0';
   options->simplex     = PETSC_TRUE;
+  options->cells[0]    = 3;
+  options->cells[1]    = 3;
+  options->cells[2]    = 3;
   options->mms         = -1;
 
   ierr = PetscOptionsBegin(comm, "", "Driven Cavity Problem Options", "DMPLEX");CHKERRQ(ierr);
   ierr = PetscOptionsInt("-dim", "The topological mesh dimension", "ex4.c", options->dim, &options->dim, NULL);CHKERRQ(ierr);
   ierr = PetscOptionsString("-f", "Mesh filename to read", "ex4.c", options->filename, options->filename, sizeof(options->filename), NULL);CHKERRQ(ierr);
   ierr = PetscOptionsBool("-simplex", "Simplicial (true) or tensor (false) mesh", "ex4.c", options->simplex, &options->simplex, NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsIntArray("-cells", "The initial mesh division", "ex12.c", options->cells, &n, NULL);CHKERRQ(ierr);
   ierr = PetscOptionsInt("-mms", "The MMS solution number", "ex4.c", options->mms, &options->mms, NULL);CHKERRQ(ierr);
   ierr = PetscOptionsEnd();
   PetscFunctionReturn(0);
@@ -301,9 +293,11 @@ static PetscErrorCode SetupParameters(AppCtx *user)
   PetscFunctionBeginUser;
   ierr = PetscBagGetData(user->params, (void **) &p);CHKERRQ(ierr);
   ierr = PetscBagSetName(user->params, "par", "Problem parameters");CHKERRQ(ierr);
-  ierr = PetscBagRegisterReal(user->params, &p->prandtl,     1.0, "prandtl",     "Prandtl number, ratio of momentum diffusivity to thermal diffusivity");CHKERRQ(ierr);
-  ierr = PetscBagRegisterReal(user->params, &p->grashof,     1.0, "grashof",     "Grashof number, ratio of bouyancy to viscous forces");CHKERRQ(ierr);
-  ierr = PetscBagRegisterReal(user->params, &p->lidvelocity, 1.0, "lidvelocity", "Shear velocity of top boundary");CHKERRQ(ierr);
+  ierr = PetscBagRegisterReal(user->params, &p->prandtl,     1.0,  "prandtl",     "Prandtl number, ratio of momentum diffusivity to thermal diffusivity");CHKERRQ(ierr);
+  ierr = PetscBagRegisterReal(user->params, &p->grashof,     1.0,  "grashof",     "Grashof number, ratio of bouyancy to viscous forces");CHKERRQ(ierr);
+  ierr = PetscBagRegisterReal(user->params, &p->lidvelocity, 1.0,  "lidvelocity", "Shear velocity of top boundary");CHKERRQ(ierr);
+  ierr = PetscBagRegisterReal(user->params, &p->alpha,       1.0,  "alpha",       "Stabilization exponent, usually in [1, 2]");CHKERRQ(ierr);
+  ierr = PetscBagRegisterReal(user->params, &p->beta,        0.03, "beta",        "Stabilization coefficient");CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
 
@@ -311,14 +305,13 @@ static PetscErrorCode CreateMesh(MPI_Comm comm, AppCtx *user, DM *dm)
 {
   PetscInt       dim      = user->dim;
   const char    *filename = user->filename;
-  PetscInt       cells[3] = {3, 3, 3};
   size_t         len;
   PetscErrorCode ierr;
 
   PetscFunctionBeginUser;
   ierr = PetscStrlen(filename, &len);CHKERRQ(ierr);
   if (!len) {
-    ierr = DMPlexCreateBoxMesh(comm, dim, user->simplex, cells, NULL, NULL, NULL, PETSC_TRUE, dm);CHKERRQ(ierr);
+    ierr = DMPlexCreateBoxMesh(comm, dim, user->simplex, user->cells, NULL, NULL, NULL, PETSC_TRUE, dm);CHKERRQ(ierr);
     ierr = PetscObjectSetName((PetscObject) *dm, "Mesh");CHKERRQ(ierr);
   } else {
     ierr = DMPlexCreateFromFile(comm, filename, PETSC_TRUE, dm);CHKERRQ(ierr);
@@ -345,8 +338,8 @@ static PetscErrorCode CreateMesh(MPI_Comm comm, AppCtx *user, DM *dm)
 static PetscErrorCode SetupProblem(PetscDS prob, AppCtx *user)
 {
   Parameter     *param;
-  PetscScalar    constants[3];
-  PetscInt       ids[4] = {1, 2, 3, 4};
+  PetscScalar    constants[10];
+  PetscInt       ids[4] = {1, 2, 3, 4}, dim;
   PetscErrorCode ierr;
 
   PetscFunctionBeginUser;
@@ -389,26 +382,213 @@ static PetscErrorCode SetupProblem(PetscDS prob, AppCtx *user)
     break;
   }
   /* Physical Constants */
+  ierr = PetscDSGetSpatialDimension(prob, &dim);CHKERRQ(ierr);
   constants[0] = param->prandtl;
   constants[1] = param->grashof;
   constants[2] = param->lidvelocity;
-  ierr = PetscDSSetConstants(prob, 3, constants);CHKERRQ(ierr);
+  constants[3] = param->alpha;
+  constants[4] = param->beta;
+  /* Computed constants */
+  constants[5] = PetscPowReal(2.0, 1.0/dim); /* diam(\Omega) for the unit cube */
+  constants[6] = 0.0;                        /* ||u||_\infty */
+  constants[7] = 0.0;                        /* var O = max_\Omega O - min_\Omega O */
+  constants[8] = 0.0;                        /* var T = max_\Omega T - min_\Omega T */
+  constants[9] = 0.0;                        /* h_K */
+  ierr = PetscDSSetConstants(prob, 10, constants);CHKERRQ(ierr);
   ierr = PetscDSSetFromOptions(prob);CHKERRQ(ierr);
   ierr = PetscPrintf(PetscObjectComm((PetscObject) prob), "lid velocity = %g, prandtl # = %g, grashof # = %g\n", (double) param->lidvelocity, (double) param->prandtl, (double) param->grashof);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
 
-static PetscErrorCode SetupMaterial(DM dm, DM dmAux, AppCtx *user)
+static void sqr_u(PetscInt dim, PetscInt Nf, PetscInt NfAux,
+                  const PetscInt uOff[], const PetscInt uOff_x[], const PetscScalar u[], const PetscScalar u_t[], const PetscScalar u_x[],
+                  const PetscInt aOff[], const PetscInt aOff_x[], const PetscScalar a[], const PetscScalar a_t[], const PetscScalar a_x[],
+                  PetscReal t, const PetscReal x[], PetscInt numConstants, const PetscScalar constants[], PetscScalar f[])
 {
-  PetscErrorCode (*funcs[1])(PetscInt dim, PetscReal time, const PetscReal x[], PetscInt Nc, PetscScalar u[], void *ctx) = {zero};
-  Vec            nu;
+  f[0] = u[0]*u[0];
+}
+
+static void identity_O(PetscInt dim, PetscInt Nf, PetscInt NfAux,
+                       const PetscInt uOff[], const PetscInt uOff_x[], const PetscScalar u[], const PetscScalar u_t[], const PetscScalar u_x[],
+                       const PetscInt aOff[], const PetscInt aOff_x[], const PetscScalar a[], const PetscScalar a_t[], const PetscScalar a_x[],
+                       PetscReal t, const PetscReal x[], PetscInt numConstants, const PetscScalar constants[], PetscScalar f[])
+{
+  f[0] = u[uOff[1]];
+}
+
+static void identity_T(PetscInt dim, PetscInt Nf, PetscInt NfAux,
+                       const PetscInt uOff[], const PetscInt uOff_x[], const PetscScalar u[], const PetscScalar u_t[], const PetscScalar u_x[],
+                       const PetscInt aOff[], const PetscInt aOff_x[], const PetscScalar a[], const PetscScalar a_t[], const PetscScalar a_x[],
+                       PetscReal t, const PetscReal x[], PetscInt numConstants, const PetscScalar constants[], PetscScalar f[])
+{
+  f[0] = u[uOff[2]];
+}
+
+/* -\Delta \Omega + U \cdot \nabla \Omega - GR \nabla_x T */
+static void residual_O(PetscInt dim, PetscInt Nf, PetscInt NfAux,
+                       const PetscInt uOff[], const PetscInt uOff_x[], const PetscScalar u[], const PetscScalar u_t[], const PetscScalar u_x[],
+                       const PetscInt aOff[], const PetscInt aOff_x[], const PetscScalar a[], const PetscScalar a_t[], const PetscScalar a_x[],
+                       PetscReal t, const PetscReal x[], PetscInt numConstants, const PetscScalar constants[], PetscScalar f[])
+{
+  const PetscReal Gr    = constants[1];
+  const PetscReal alpha = constants[3];
+  PetscReal       res   = 0.0;
+  PetscInt        d;
+
+  /* TODO Calculate strong Laplacian */
+  for (d = 0; d < dim; ++d) res += u[d] * u_x[uOff_x[1]+d];
+  res -= Gr * u_x[uOff_x[2]+0];
+  f[0] = PetscAbsScalar(res) * PetscPowReal(u[1], alpha - 1.0);
+}
+
+/* -\Delta T + PR U \cdot \nabla T */
+static void residual_T(PetscInt dim, PetscInt Nf, PetscInt NfAux,
+                       const PetscInt uOff[], const PetscInt uOff_x[], const PetscScalar u[], const PetscScalar u_t[], const PetscScalar u_x[],
+                       const PetscInt aOff[], const PetscInt aOff_x[], const PetscScalar a[], const PetscScalar a_t[], const PetscScalar a_x[],
+                       PetscReal t, const PetscReal x[], PetscInt numConstants, const PetscScalar constants[], PetscScalar f[])
+{
+  const PetscReal Pr    = constants[0];
+  const PetscReal alpha = constants[3];
+  PetscReal       res   = 0.0;
+  PetscInt        d;
+
+  /* TODO Calculate strong Laplacian */
+  for (d = 0; d < dim; ++d) res += Pr * u[d] * u_x[uOff_x[2]+d];
+  f[0] = PetscAbsScalar(res) * PetscPowReal(u[2], alpha - 1.0);
+}
+
+static void entropyViscosityO(PetscInt dim, PetscInt Nf, PetscInt NfAux,
+                              const PetscInt uOff[], const PetscInt uOff_x[], const PetscScalar u[], const PetscScalar u_t[], const PetscScalar u_x[],
+                              const PetscInt aOff[], const PetscInt aOff_x[], const PetscScalar a[], const PetscScalar a_t[], const PetscScalar a_x[],
+                              PetscReal t, const PetscReal x[], PetscInt numConstants, const PetscScalar constants[], PetscScalar nu[])
+{
+  /* \nu_\alpha(T)_K = \beta \|\|u\|\|_{\infty(K)} \mathrm{min} \left( h_K, h^\alpha_K \frac{\|\|R_\alpha(T)\|\|_{\infty(K)}}{c(u,T)} \right) */
+  /* c(u,T)          = c_R \|\|u\|\|_{\infty(\Omega)} var(T) |diam(\Omega)|^{\alpha−2} */
+  const PetscReal alpha    = constants[3];
+  const PetscReal beta     = constants[4];
+  const PetscReal diam     = constants[5];
+  const PetscReal umax     = constants[6];
+  const PetscReal varO     = constants[7];
+  const PetscReal h        = constants[9];
+  const PetscReal c_R      = PetscPowReal(2.0, (4.0 - 2.0*alpha)/dim);
+  const PetscReal umaxK    = a[0];
+  const PetscReal RO_alpha = a[1];
+
+  nu[0] = beta * umaxK * PetscMin(h, PetscPowReal(h, alpha)*(RO_alpha/(c_R * umax * varO * PetscPowReal(diam, alpha-2.0))));
+}
+
+static void entropyViscosityT(PetscInt dim, PetscInt Nf, PetscInt NfAux,
+                              const PetscInt uOff[], const PetscInt uOff_x[], const PetscScalar u[], const PetscScalar u_t[], const PetscScalar u_x[],
+                              const PetscInt aOff[], const PetscInt aOff_x[], const PetscScalar a[], const PetscScalar a_t[], const PetscScalar a_x[],
+                              PetscReal t, const PetscReal x[], PetscInt numConstants, const PetscScalar constants[], PetscScalar nu[])
+{
+  /* \nu_\alpha(T)_K = \beta \|\|u\|\|_{\infty(K)} \mathrm{min} \left( h_K, h^\alpha_K \frac{\|\|R_\alpha(T)\|\|_{\infty(K)}}{c(u,T)} \right) */
+  /* c(u,T)          = c_R \|\|u\|\|_{\infty(\Omega)} var(T) |diam(\Omega)|^{\alpha−2} */
+  const PetscReal alpha    = constants[3];
+  const PetscReal beta     = constants[4];
+  const PetscReal diam     = constants[5];
+  const PetscReal umax     = constants[6];
+  const PetscReal varT     = constants[8];
+  const PetscReal h        = constants[9];
+  const PetscReal c_R      = PetscPowReal(2.0, (4.0 - 2.0*alpha)/dim);
+  const PetscReal umaxK    = a[0];
+  const PetscReal RT_alpha = a[2];
+
+  nu[0] = beta * umaxK * PetscMin(h, PetscPowReal(h, alpha)*(RT_alpha/(c_R * umax * varT * PetscPowReal(diam, alpha-2.0))));
+}
+
+static PetscErrorCode UpdateEntropyViscosity(SNES snes, PetscInt it)
+{
+  DM             dm, dmAux, dmMax;
+  PetscDS        prob;
+  Vec            u, nu, umax;
+  PetscReal      maxval[3];
+  PetscScalar   *constants;
   PetscErrorCode ierr;
 
-  PetscFunctionBegin;
+  PetscFunctionBeginUser;
+  ierr = SNESGetDM(snes, &dm);CHKERRQ(ierr);
+  ierr = SNESGetSolution(snes, &u);CHKERRQ(ierr);
+
+  ierr = PetscObjectQuery((PetscObject) dm, "dmAux", (PetscObject *) &dmAux);CHKERRQ(ierr);
+  ierr = PetscObjectQuery((PetscObject) dm, "A", (PetscObject *) &nu);CHKERRQ(ierr);
+
+  ierr = PetscObjectQuery((PetscObject) dmAux, "dmAux", (PetscObject *) &dmMax);CHKERRQ(ierr);
+  ierr = PetscObjectQuery((PetscObject) dmAux, "A", (PetscObject *) &umax);CHKERRQ(ierr);
+
+  ierr = DMGetDS(dm, &prob);CHKERRQ(ierr);
+  ierr = PetscDSSetObjective(prob, 0, sqr_u);CHKERRQ(ierr);
+  ierr = PetscDSSetObjective(prob, 1, identity_O);CHKERRQ(ierr);
+  ierr = PetscDSSetObjective(prob, 2, identity_T);CHKERRQ(ierr);
+  ierr = DMPlexComputeCellwiseMaxFEM(dm, u, umax, NULL);CHKERRQ(ierr);
+
+  ierr = VecStrideMaxAll(umax, NULL, maxval);CHKERRQ(ierr);
+  /* TODO Also calculate minimums, for now use 0 */
+  ierr = PetscDSGetConstants(prob, NULL, (const PetscScalar **) &constants);CHKERRQ(ierr);
+  constants[6] = PetscSqrtReal(maxval[0]); /* ||u||_\infty */
+  constants[7] = maxval[1] - 0.0;          /* var O = max_\Omega O - min_\Omega O */
+  constants[8] = maxval[2] - 0.0;          /* var T = max_\Omega T - min_\Omega T */
+
+  ierr = PetscDSSetObjective(prob, 0, sqr_u);CHKERRQ(ierr);
+  ierr = PetscDSSetObjective(prob, 1, residual_O);CHKERRQ(ierr);
+  ierr = PetscDSSetObjective(prob, 2, residual_T);CHKERRQ(ierr);
+  ierr = DMPlexComputeCellwiseMaxFEM(dm, u, umax, NULL);CHKERRQ(ierr);
+
+  ierr = PetscDSSetObjective(prob, 0, entropyViscosityO);CHKERRQ(ierr);
+  ierr = PetscDSSetObjective(prob, 1, entropyViscosityT);CHKERRQ(ierr);
+  ierr = DMPlexComputeCellwiseMaxFEM(dmAux, nu, nu, NULL);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+static PetscErrorCode SetupMaterial(DM dm, DM dmAux, AppCtx *user)
+{
+  DM             dmMax;
+  Vec            nu, umax;
+  PetscErrorCode ierr;
+
+  PetscFunctionBeginUser;
   ierr = DMCreateLocalVector(dmAux, &nu);CHKERRQ(ierr);
-  ierr = DMProjectFunctionLocal(dmAux, 0.0, funcs, NULL, INSERT_ALL_VALUES, nu);CHKERRQ(ierr);
+  ierr = VecSet(nu, 0.0);CHKERRQ(ierr);
   ierr = PetscObjectCompose((PetscObject) dm, "A", (PetscObject) nu);CHKERRQ(ierr);
   ierr = VecDestroy(&nu);CHKERRQ(ierr);
+
+  ierr = DMClone(dm, &dmMax);CHKERRQ(ierr);
+  {
+    MPI_Comm        comm;
+    PetscDS         prob;
+    PetscFE         feU, feO, feT;
+    PetscQuadrature q;
+    PetscInt        dim;
+    PetscBool       simplex = user->simplex;
+
+    ierr = PetscObjectGetComm((PetscObject) dm, &comm);CHKERRQ(ierr);
+    ierr = DMGetDimension(dm, &dim);CHKERRQ(ierr);
+    ierr = PetscFECreateDefault(comm, dim, 1, simplex, "velmax_", -1, &feU);CHKERRQ(ierr);
+    ierr = PetscFEGetQuadrature(feU, &q);CHKERRQ(ierr);
+    ierr = PetscObjectSetName((PetscObject) feU, "velocity max");CHKERRQ(ierr);
+
+    ierr = PetscFECreateDefault(comm, dim, 1, simplex, "vortmax_", -1, &feO);CHKERRQ(ierr);
+    ierr = PetscFESetQuadrature(feO, q);CHKERRQ(ierr);
+    ierr = PetscObjectSetName((PetscObject) feO, "vorticity max");CHKERRQ(ierr);
+
+    ierr = PetscFECreateDefault(comm, dim, 1, simplex, "tempmax_", -1, &feT);CHKERRQ(ierr);
+    ierr = PetscFESetQuadrature(feT, q);CHKERRQ(ierr);
+    ierr = PetscObjectSetName((PetscObject) feT, "temperature max");CHKERRQ(ierr);
+
+    ierr = DMGetDS(dmMax, &prob);CHKERRQ(ierr);
+    ierr = PetscDSSetDiscretization(prob, 0, (PetscObject) feU);CHKERRQ(ierr);
+    ierr = PetscDSSetDiscretization(prob, 1, (PetscObject) feO);CHKERRQ(ierr);
+    ierr = PetscDSSetDiscretization(prob, 2, (PetscObject) feT);CHKERRQ(ierr);
+
+    ierr = PetscFEDestroy(&feU);CHKERRQ(ierr);
+    ierr = PetscFEDestroy(&feO);CHKERRQ(ierr);
+    ierr = PetscFEDestroy(&feT);CHKERRQ(ierr);
+  }
+  ierr = DMCreateLocalVector(dmMax, &umax);CHKERRQ(ierr);
+  ierr = PetscObjectCompose((PetscObject) dmAux, "dmAux", (PetscObject) dmMax);CHKERRQ(ierr);
+  ierr = PetscObjectCompose((PetscObject) dmAux, "A", (PetscObject) umax);CHKERRQ(ierr);
+  ierr = DMDestroy(&dmMax);CHKERRQ(ierr);
+  ierr = VecDestroy(&umax);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
 
@@ -450,6 +630,7 @@ static PetscErrorCode SetupDiscretization(DM dm, AppCtx *user)
 
   ierr = PetscDSCreate(PetscObjectComm((PetscObject) dm), &probAux);CHKERRQ(ierr);
   ierr = PetscDSSetDiscretization(probAux, 0, (PetscObject) feNu);CHKERRQ(ierr);
+  ierr = PetscDSSetDiscretization(probAux, 1, (PetscObject) feNu);CHKERRQ(ierr);
   while (cdm) {
     DM coordDM, dmAux;
 
@@ -459,6 +640,7 @@ static PetscErrorCode SetupDiscretization(DM dm, AppCtx *user)
     ierr = DMClone(cdm, &dmAux);CHKERRQ(ierr);
     ierr = DMSetCoordinateDM(dmAux, coordDM);CHKERRQ(ierr);
     ierr = DMSetDS(dmAux, probAux);CHKERRQ(ierr);
+    ierr = PetscDSDestroy(&probAux);CHKERRQ(ierr);
     ierr = PetscObjectCompose((PetscObject) dm, "dmAux", (PetscObject) dmAux);CHKERRQ(ierr);
     ierr = SetupMaterial(cdm, dmAux, user);CHKERRQ(ierr);
     ierr = DMDestroy(&dmAux);CHKERRQ(ierr);
@@ -495,12 +677,13 @@ int main(int argc, char **argv)
   ierr = SetupParameters(&user);CHKERRQ(ierr);
   ierr = SetupDiscretization(dm, &user);CHKERRQ(ierr);
   ierr = DMPlexSetSNESLocalFEM(dm, &user, &user, &user);CHKERRQ(ierr);
+  //ierr = SNESSetUpdate(snes, UpdateEntropyViscosity);CHKERRQ(ierr);
   ierr = SNESSetFromOptions(snes);CHKERRQ(ierr);
 
   ierr = DMCreateGlobalVector(dm, &x);CHKERRQ(ierr);
   ierr = PetscObjectSetName((PetscObject) x, "solution");CHKERRQ(ierr);
   ierr = DMProjectFunction(dm, 0.0, initialGuesses, NULL, INSERT_ALL_VALUES, x);CHKERRQ(ierr);
-  ierr = DMSNESCheckFromOptions(snes, x, NULL, NULL);CHKERRQ(ierr);
+  if (user.mms >= 0) {ierr = DMSNESCheckFromOptions(snes, x, NULL, NULL);CHKERRQ(ierr);}
   ierr = SNESSolve(snes, NULL, x);CHKERRQ(ierr);
   ierr = VecViewFromOptions(x, NULL, "-sol_view");CHKERRQ(ierr);
 
