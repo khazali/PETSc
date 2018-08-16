9f4d4711fb9673a01d691109c3dc1d99cfc4d3a8
Junchao Zhang <jczhang@mcs.anl.gov>
2018-08-16 14:27:18 -0500

    Update vpscat.c/h to use the new shared memory communicator interface

--- src/vec/vscat/impls/vpscat.h
+++ src/vec/vscat/impls/vpscat.h
@@ -340,14 +340,14 @@ PetscErrorCode PETSCMAP1(VecScatterEndMPI3Node)(VecScatter ctx,Vec xin,Vec yin,I
   Vec_Node               *vnode;
   PetscInt               cnt,*idx,*idy;
   MPI_Comm               comm,mscomm,veccomm;
-  PetscCommShared        scomm;
+  PetscShmComm           scomm;
 
   PetscFunctionBegin;
   if (mode & SCATTER_LOCAL) PetscFunctionReturn(0);
 
   ierr = PetscObjectGetComm((PetscObject)ctx,&comm);CHKERRQ(ierr);
-  ierr = PetscCommSharedGet(comm,&scomm);CHKERRQ(ierr);
-  ierr = PetscCommSharedGetComm(scomm,&mscomm);CHKERRQ(ierr);
+  ierr = PetscShmCommGet(comm,&scomm);CHKERRQ(ierr);
+  ierr = PetscShmCommGetMpiShmComm(scomm,&mscomm);CHKERRQ(ierr);
 
   ierr = VecGetArray(yin,&yv);CHKERRQ(ierr);
 
