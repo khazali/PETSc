d95a42c4d04b89bae587c7d3a18b407507394140
Toby Isaac <tisaac@cc.gatech.edu>
2018-11-11 18:04:39 -0500

    DMP4est: update to p4est that handles empty processor bug

--- config/BuildSystem/config/packages/p4est.py
+++ config/BuildSystem/config/packages/p4est.py
@@ -3,7 +3,7 @@ import config.package
 class Configure(config.package.GNUPackage):
   def __init__(self, framework):
     config.package.GNUPackage.__init__(self, framework)
-    self.gitcommit         = '9a2c5dedd19c9724f4f22786d489abf75ba4a2d4'
+    self.gitcommit         = 'e5a05c1cf466faba47798529aff86a8eea9c2a52'
     self.download          = ['git://https://github.com/tisaac/p4est','https://github.com/p4est/p4est.github.io/raw/master/release/p4est-2.0.tar.gz']
     self.functions         = ['p4est_init']
     self.includes          = ['p4est_bits.h']
