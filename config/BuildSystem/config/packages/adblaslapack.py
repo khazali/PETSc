1245d449854c75f78d0760f35696b70947fff4d2
Barry Smith <bsmith@mcs.anl.gov>
2018-11-19 16:17:56 -0600

    Update to use const version of adblaslapack
    Commit-type: feature

--- config/BuildSystem/config/packages/adblaslapack.py
+++ config/BuildSystem/config/packages/adblaslapack.py
@@ -3,7 +3,7 @@ import config.package
 class Configure(config.package.Package):
   def __init__(self, framework):
     config.package.Package.__init__(self, framework)
-    self.gitcommit  = 'master'
+    self.gitcommit  = 'origin/barry/add-const'
     self.download   = ['git://https://xgitlab.cels.anl.gov/schanen/adblaslapack.git']
     self.functions  = []
     self.includes   = []
